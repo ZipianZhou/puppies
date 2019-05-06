45cc18f07c96dfd747de0376930d5a053083d36b
Yifan Wang <yifanwangsh@yahoo.com>
2019-05-06 12:01:46 -0400

    some try on models

--- users/models.py
+++ users/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
 
-
-class User(AbstractUser):
-    nickname = models.CharField(max_length=50, blank=True)
-
-    class Meta(AbstractUser.Meta):
-        pass
+    objects = models.Manager()
